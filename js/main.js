35f3da3458e53dbc7f3d0f894777d7b3f5a51ba5
gavinblair <gavin.blair@rtraction.com>
2013-02-15 16:36:18 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 $(document).ready(function($) {
 
 	var w = new weather('160caaa27c885952');
-	w.get_current();
-	console.log(w);
-	$("div").text(w.i);
 
-	/*eat_me = w.get_current();
-	console.log(eat_me);
-	eat_me = eat_me.weather;*/
-
-	//$('div').text(eat_me);
-
-
+	w.get_current(function(data){
+		var weather_template = ich.weather({
+			"feelslike": data.celcius.feelslike,
+			"weather": data.weather
+		});
+		$("#container").html(weather_template);
+	});
 
 });
