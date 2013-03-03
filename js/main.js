fa4384fab43b8f78a85e2fe1cf28a1d7ec3635e8
gavinblair <gavin.blair@rtraction.com>
2013-03-02 23:34:39 -0500

    actual temp

--- js/main.js
+++ js/main.js
@@ -1,41 +1,56 @@
-var weather;
-$(document).ready(function($) {
+var weatherbacon = function(){
+	var weather;
+	$(document).ready(function($) {
 
-	/* prevent "0" being on the screen while we wait for a new value */
-	document.getElementById("temp").style.visibility = 'visible';
-	
-	var temp = new numfader(document.getElementById("temp"));
-	temp.animateText(0, 34, 10, function(){
-		$(temp.el).addClass('animated bounceIn');
-	});
+		weather = new wunderground('160caaa27c885952', 'metric');
 
-	/*
-	
-	weather = new wunderground('160caaa27c885952', 'metric');
+		weather.updateCurrent(function(current){
 
-	weather.updateCurrent(function(current){
+			var weather_template = ich.weather({
+				"feelslike": current.feelslike,
+				"english": current.english,
+				"forecast": [
+					{
+						index: 1,
+						letter: 'M',
+						high: '36'
+					},
+					{
+						index: 1,
+						letter: 'M',
+						high: '36'
+					}
+				],
+				"unit": weather.unit.code.toUpperCase()
+			});
 
-		var weather_template = ich.weather({
-			"feelslike": current.feelslike,
-			"english": current.english,
-			"unit": weather.unit.code.toUpperCase()
+			$("#container").html(weather_template);
+
+			/* prevent "0" being on the screen while we wait for a new value */
+			document.getElementById("temp").style.visibility='visible';
+
+			var tempEffect = new numfader(document.getElementById("temp"));
+			var interval = 10;
+			if(Math.abs(parseInt(current.feelslike,10)) < 10) {
+				interval = 100;
+			}
+			tempEffect.animateText(0, parseInt(current.feelslike,10), interval, function(){
+				$(tempEffect.el).addClass('animated bounceIn');
+			});
+
+
 		});
-		$("#current").html(weather_template);
 
+		/*weather.updateForecast(function(forecast){
 
-		
-	});
+			var weather_template = ich.weather({
 
-	weather.updateForecast(function(forecast){
+			});
 
-		var weather_template = ich.weather({
+			$("#forecast").html(weather_template);
 			
-		});
+		});*/
 
-		$("#forecast").html(weather_template);
-		
 	});
-	*/
-	
-
-});
+};
+var app = new weatherbacon();
